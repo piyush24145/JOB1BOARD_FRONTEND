2b6ba204d1fc45a7a0aa5fb9faaaa4d5e27fb646
piyush kumar <24145@iiitu.ac.in>
2026-03-21 01:27:15 +0530

    Fix import case for Mosaic component

--- src/Candidate/Pages/register/Register.jsx
+++ src/Candidate/Pages/register/Register.jsx
@@ -3,8 +3,7 @@ import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link, useNavigate } from "react-router-dom";
 import API from "/src/api/axios";
-import Mosaic from "../login/Loginsection/mosaic";
-
+import Mosaic from "../login/Loginsection/Mosaic";
 const Register = () => {
   const navigate = useNavigate();
 
